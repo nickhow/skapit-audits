404a3905d2c3c420e3deaadccb98e6c1d8df5f67
nickhow <hownick7@gmail.com>
2022-04-04 10:45:54 +0100

    Update for Live
    Remove backups from git files
    add live credentials / db

--- appstarter/backup_app_21-03/Views/templates/footer.php
+++
@@ -1,9 +0,0 @@
-
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col-12">Health and Safety Audits powered by SKAPIT</div>
-            </div>
-        </div>
-    
-    </body>
-</html>
