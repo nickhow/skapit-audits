7d0fff974a1cd6f08fcadff0c7c6abcf03b4efa7
nickhow <hownick7@gmail.com>
2022-04-07 15:40:23 +0100

    Update add_audit.php

--- appstarter/app/Views/add_audit.php
+++ appstarter/app/Views/add_audit.php
@@ -83,8 +83,8 @@
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog model-lg" style="max-width:min-content">
-    <div class="modal-content" style="width:min-content">
+  <div class="modal-dialog modal-lg" >
+    <div class="modal-content" >
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Email Preview</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
