cbdefabec6dfe2bf3224c1f744aa037a28eda6a0
nickhow <hownick7@gmail.com>
2022-04-07 11:24:58 +0100

    json

--- appstarter/app/Controllers/EmailController.php
+++ appstarter/app/Controllers/EmailController.php
@@ -8,7 +8,7 @@ class EmailController extends Controller
     // Get Email HTML
     public function getEmailHtml($type,$lang) {
         $emailModel = new EmailModel();
-        echo json_encode($emailModel->getEmailHtml($type,$lang));
+        echo ($emailModel->getEmailHtml($type,$lang));
     }
     
 }
