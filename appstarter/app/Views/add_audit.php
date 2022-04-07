8ec16f30e7ab7b0535b301c925e30dbfe8368210
nickhow <hownick7@gmail.com>
2022-04-07 12:19:04 +0100

    Update add_audit.php

--- appstarter/app/Views/add_audit.php
+++ appstarter/app/Views/add_audit.php
@@ -74,12 +74,28 @@
             </form>
         </div>
 
-        <div class="d-none d-lg-block col-5">
-          <h3>Email Preview</h3>
-          <div class="col-12 border p-4">
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Preview Email
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Email Preview</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="col-10 border p-4">
             <div id="showEmail"></div>
-          </div>
         </div>
+      </div>
+
+    </div>
+  </div>
+</div>
 
     </div>
   </div>
