9ecd1986fb1107168bc62a4adff8b5f8494776b8
nickhow <hownick7@gmail.com>
2022-04-11 13:18:48 +0100

    Update AuditCrud.php

--- appstarter/app/Controllers/AuditCrud.php
+++ appstarter/app/Controllers/AuditCrud.php
@@ -355,7 +355,7 @@ class AuditCrud extends Controller
         $account_id = $this->request->getVar('account');      
         
         if($account_id == ""){
-            $session->setFlashdata('msg', "Can't send an audit without a property, please create a property first. </br> <a href=".site_url('/account/new').">Create New Property</a>);
+            $session->setFlashdata('msg', "Can't send an audit without a property, please create a property first. </br> <a href=".site_url('/account/new').">Create New Property</a>");
             return $this->response->redirect(site_url('/audit/new'));
         }
 
