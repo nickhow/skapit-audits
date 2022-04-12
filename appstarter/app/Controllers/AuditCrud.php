da57a647fc344947860a4bd68d05d2619332bda2
nickhow <hownick7@gmail.com>
2022-04-12 15:24:55 +0100

    Update AuditCrud.php

--- appstarter/app/Controllers/AuditCrud.php
+++ appstarter/app/Controllers/AuditCrud.php
@@ -1274,7 +1274,7 @@ class AuditCrud extends Controller
         ]);
         $mpdf->WriteHTML($html);
 
-        $filename = "./".$audit_id."/results.pdf";
+        $filename = "../writable/".$audit_id."/results.pdf";
 
         $mpdf->Output($filename, 'F');
 
