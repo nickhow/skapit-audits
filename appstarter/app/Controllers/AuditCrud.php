a1af9f8367ea757c9ab01e98c259d3e0734bdd45
nickhow <hownick7@gmail.com>
2022-04-12 11:20:41 +0100

    Update AuditCrud.php

--- appstarter/app/Controllers/AuditCrud.php
+++ appstarter/app/Controllers/AuditCrud.php
@@ -1326,16 +1326,16 @@ class AuditCrud extends Controller
         $mpdf = new \Mpdf\Mpdf([
             'mode' => 'utf-8',
             'format' => 'A4',
-            'margin_left' => 0,
-            'margin_right' => 0,
-            'margin_top' => 0,
-            'margin_bottom' => 0,
+            'margin_left' => 2.5,
+            'margin_right' => 2.5,
+            'margin_top' => 2.5,
+            'margin_bottom' => 02.5,
             'margin_header' => 0,
             'margin_footer' => 0
         ]);
         $mpdf->WriteHTML($html);
 
-        $mpdf->Output('example.pdf', 'F');
+        $mpdf->Output('results/'.$audit_id.'.pdf', 'F');
 
 
     //    $email_content = "Test";
