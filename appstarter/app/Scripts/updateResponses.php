efbc0eeb168986d9dc1a5af71d6e8d84340eab7a
nickhow <hownick7@gmail.com>
2022-05-13 11:57:14 +0100

    Update updateResponses.php

--- appstarter/app/Scripts/updateResponses.php
+++ appstarter/app/Scripts/updateResponses.php
@@ -15,15 +15,18 @@ if (!$conn) {
 $sql_select = "SELECT DISTINCT id FROM audits WHERE `status` IN ('complete','reviewed','reviewing')";
 $result = mysqli_query($conn, $sql_select);
 $ids = mysqli_fetch_array($result,MYSQLI_NUM);
+
+print_r($ids)
+
     //generate the basic response to satisfy the new question
-    foreach($ids as $id){
-        $sql_insert = "INSERT INTO responses (audit_id, question_id, answer_id, suggested_score_ba, suggested_score_abta, custom_answer) VALUES('$id','129','10002','0','0','')";
-        if(mysqli_query($conn, $sql_insert)){
-            echo "Insert success for id: ".$id;
-        } else {
-            echo "Failed to insert for id: ".$id;
-        }
-    }
+ //   foreach($ids as $id){
+ //       $sql_insert = "INSERT INTO responses (audit_id, question_id, answer_id, suggested_score_ba, suggested_score_abta, custom_answer) VALUES('$id','129','10002','0','0','')";
+ //       if(mysqli_query($conn, $sql_insert)){
+ //           echo "Insert success for id: ".$id;
+  //      } else {
+  //          echo "Failed to insert for id: ".$id;
+  //      }
+  //  }
 
 
 
