cb3ddf98c6c862895a0a93d0be27147860267d5b
nickhow <hownick7@gmail.com>
2022-05-13 11:01:24 +0100

    Create updateResponses.php

---
+++ appstarter/app/Scripts/updateResponses.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Models\ResponseModel;
+$db = db_connect();
+
+//get the unique IDs from the responses table
+$ids = $db->query("SELECT audit_id FROM `audits` WHERE `status` IN ('reviewed','reviewing','complete')")->getResult();
+
+//generate the basic response to satisfy the new question
+$responseModel = new ResponseModel();
+foreach($ids as $id){
+    $response = [
+        'audit_id' => $id,
+        'question_id' => 129,  
+        'answer_id' => 10002,
+        'suggested_score_ba' => 0,
+        'suggested_score_abta' => 0,
+        'custom_answer' => "",
+    ];
+
+    $responseModel->insert($response);
+}
+
+?>
