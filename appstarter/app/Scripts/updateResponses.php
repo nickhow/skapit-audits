b45764cc035886291f85b90cd9a9e2924496bfd7
nickhow <hownick7@gmail.com>
2022-05-13 11:08:59 +0100

    Update updateResponses.php

--- appstarter/app/Scripts/updateResponses.php
+++ appstarter/app/Scripts/updateResponses.php
@@ -3,25 +3,26 @@ namespace App\Controllers;
 use App\Models\ResponseModel;
 class AuditCrud extends Controller
 {
+    function main(){
+        $db = db_connect();
 
-    $db = db_connect();
+        //get the unique IDs from the responses table
+        $ids = $db->query("SELECT audit_id FROM `audits` WHERE `status` IN ('reviewed','reviewing','complete')")->getResult();
 
-    //get the unique IDs from the responses table
-    $ids = $db->query("SELECT audit_id FROM `audits` WHERE `status` IN ('reviewed','reviewing','complete')")->getResult();
+        //generate the basic response to satisfy the new question
+        $responseModel = new ResponseModel();
+        foreach($ids as $id){
+            $response = [
+                'audit_id' => $id,
+                'question_id' => 129,  
+                'answer_id' => 10002,
+                'suggested_score_ba' => 0,
+                'suggested_score_abta' => 0,
+                'custom_answer' => "",
+            ];
 
-    //generate the basic response to satisfy the new question
-    $responseModel = new ResponseModel();
-    foreach($ids as $id){
-        $response = [
-            'audit_id' => $id,
-            'question_id' => 129,  
-            'answer_id' => 10002,
-            'suggested_score_ba' => 0,
-            'suggested_score_abta' => 0,
-            'custom_answer' => "",
-        ];
-
-        $responseModel->insert($response);
+            $responseModel->insert($response);
+        }
     }
 }
 ?>
