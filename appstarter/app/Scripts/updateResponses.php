09ae69d4c8ea984959fc85c9d0d4fc06a0731640
nickhow <hownick7@gmail.com>
2022-05-13 11:30:35 +0100

    Update updateResponses.php

--- appstarter/app/Scripts/updateResponses.php
+++ appstarter/app/Scripts/updateResponses.php
@@ -13,11 +13,15 @@ if (!$conn) {
 
 //get the unique IDs from the responses table
 $sql_select = "SELECT audit_id FROM `audits` WHERE `status` IN ('reviewed','reviewing','complete')";
-$ids = mysqli_query($conn, $sql_select);
-
-//generate the basic response to satisfy the new question
-foreach($ids as $id){
-    $sql_insert = "INSERT INTO responses (audit_id, question_id, answer_id, suggested_score_ba, suggested_score_abta, custom_answer) VALUES('$id',129,10002,0,0,'')";
-    mysqli_query($conn, $sql_insert);
+if($ids = mysqli_query($conn, $sql_select)){
+    //generate the basic response to satisfy the new question
+    foreach($ids as $id){
+        $sql_insert = "INSERT INTO responses (audit_id, question_id, answer_id, suggested_score_ba, suggested_score_abta, custom_answer) VALUES($id,129,10002,0,0,'')";
+        mysqli_query($conn, $sql_insert);
+    }
+} else {
+    echo "failed to get IDs";
 }
+
+
 ?>
