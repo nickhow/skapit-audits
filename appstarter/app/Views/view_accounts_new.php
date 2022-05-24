a6adb9781da73e97b76eeac956d6e72a8a735189
nickhow <hownick7@gmail.com>
2022-05-24 18:02:43 +0100

    Update view_accounts_new.php

--- appstarter/app/Views/view_accounts_new.php
+++ appstarter/app/Views/view_accounts_new.php
@@ -62,9 +62,9 @@
                         <span class="text-danger text-center"><?php echo ucFirst($audit['result_abta']); ?></span>
                     <?php else: ?>
                         <span class="text-warning text-center">Unknown</span>
-                    <?php endif ?>
+                    <?php endif 
                  } else {
-                    echo "Unknown"
+                     echo "Unknown" 
                 }
                 ?>
               </td> 
