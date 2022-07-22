bd2ab78a593688cca5eec215afde5f853b5ed3de
nickhow <hownick7@gmail.com>
2022-07-22 10:56:33 +0100

    test

--- appstarter/app/Controllers/SignupController.php
+++ appstarter/app/Controllers/SignupController.php
@@ -226,10 +226,10 @@ class SignupController extends Controller
 
         // check email
         $email_to_reset = $this->request->getVar('email');
-      //  $user_to_reset = $userModel->where('email',$email_to_reset)->first();
-      //  if( is_null( $user_to_reset ) ) {
+        $user_to_reset = $userModel->where('user_email',$email_to_reset)->first();
+        if( is_null( $user_to_reset ) ) {
             //stop here - not a user
-     //   };
+        };
         print_r($email_to_reset);
 
         // create a reset row and remove any old ones for this account
