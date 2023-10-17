02e851b6a3a3a0808cf6a27df4d360f02f0f7b29
nickhow <hownick7@gmail.com>
2023-10-17 14:37:01 +0100

    Update Routes.php

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -79,8 +79,8 @@ $routes->get('audit/(:alphanum)/delete', 'AuditCrud::delete/$1');
 $routes->post('audit/(:alphanum)/pay', 'AuditCrud::markPaid/$1');
 
 // these are temporary ones.
-$routes->get('audit/rescore/(:alphanum)', 'AuditCrud::scoreeasyJet/$1');
-$routes->get('audit/total/(:alphanum)', 'AuditCrud::easyJetTotals/$1');
+$routes->get('rescore/(:alphanum)', 'AuditCrud::scoreeasyJet/$1');
+$routes->get('total/(:alphanum)', 'AuditCrud::easyJetTotals/$1');
 
 $routes->get('audits/hc', 'AuditCrud::index/7/true');
 
