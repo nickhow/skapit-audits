be555c209b7877395098643a81be1d8c7fc24df8
nickhow <hownick7@gmail.com>
2025-04-29 15:27:41 +0100

    Update Routes.php

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -6,7 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->setAutoRoute(true);
-$routes->setTranslateURIDashes(true);
+$routes->setTranslateURIDashes(false);
 
 $routes->get('/', 'Home::index');
 $routes->get('/', 'AuditCrud::index');
