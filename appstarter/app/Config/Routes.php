0448d5af0cb41c4084267d11d492acf2fe2ecb64
nickhow <hownick7@gmail.com>
2025-04-29 14:40:55 +0100

    Update Routes.php
    use auto-route (legacy support) to get the thing running and reduce upgrades for the moment

--- appstarter/app/Config/Routes.php
+++ appstarter/app/Config/Routes.php
@@ -5,6 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setAutoRoute(true);
+
 $routes->get('/', 'Home::index');
 $routes->get('/', 'AuditCrud::index');
 $routes->get('', 'AuditCrud::index');
