3de7f524be34709d35939d94a2a47fb78d392180
nickhow <hownick7@gmail.com>
2025-05-14 10:02:20 +0100

    Update SigninController.php

--- appstarter/app/Controllers/SigninController.php
+++ appstarter/app/Controllers/SigninController.php
@@ -54,7 +54,7 @@ class SigninController extends Controller
                 
                 $session->set($ses_data);
                 
-                if(isset($uri) && !$uri == "" ) {
+                if(isset($uri) && !$uri == "" && !$uri == "https://simple-audit.com" ) {
                    
                     $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
                     $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
