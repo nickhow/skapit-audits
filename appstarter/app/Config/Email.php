f29d27277fb9a51e5bd4715da490ead5355b1fd5
nickhow <hownick7@gmail.com>
2026-02-04 23:56:51 +0000

    Update Email.php

--- appstarter/app/Config/Email.php
+++ appstarter/app/Config/Email.php
@@ -33,12 +33,12 @@ class Email extends BaseConfig
     /**
      * SMTP Username
      */
-    public string $SMTPUser = env('email.smtpUser');;
+    public string $SMTPUser = env('email.smtpUser');
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = env('email.smtpPass');;
+    public string $SMTPPass = env('email.smtpPass');
 
     /**
      * SMTP Port
